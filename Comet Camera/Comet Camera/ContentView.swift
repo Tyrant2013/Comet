eab26acafe7bcae221ef3950d8fdf15e51ad7082
Zhuang Xiaowei <zhuangxiaowei_dev@163.com>
2026-03-17 15:40:26 +0800

    demo

--- Comet Camera/Comet Camera/ContentView.swift
+++ Comet Camera/Comet Camera/ContentView.swift
@@ -30,30 +30,56 @@ struct ContentView: View {
                         .frame(width: width + 10, height: height + 10)
                         .background(Color.black)
                         .clipShape(.rect(cornerRadius: 14))
+                        .overlay(
+                            CCLensBar(lensControl: viewModel.lensControl)
+                                .padding(.bottom, 24)
+                            , alignment: .bottom
+                        )
                         .frame(width: proxy.size.width, height: proxy.size.height, alignment: .top)
                 }
                 VStack(spacing: 0) {
                     Spacer()
-                    CCLensBar(lensControl: viewModel.lensControl)
-                        .padding(.bottom, 24)
-                    
-                    Button(action: {}) {
-                        Circle()
-                            .foregroundStyle(.white)
-                            .padding(5)
+                    ZStack {
+                        Button(action: {}) {
+                            ZStack {
+                                Color.orange
+                                    .clipShape(.rect(cornerRadius: 8))
+                                RoundedRectangle(cornerRadius: 8)
+                                    .stroke(lineWidth: 2)
+                                    .foregroundStyle(.white)
+                            }
+                            .frame(width: 50, height: 50)
+                        }
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading, 50)
+                        
+                        Button(action: {}) {
+                            Circle()
+                                .foregroundStyle(.white)
+                                .padding(5)
+                        }
+                        .frame(width: 80, height: 80)
+                        .background(
+                            Circle()
+                                .stroke(Color.white, lineWidth: 5)
+                        )
+                        
+                        Button(action: {}) {
+                            Image(systemName: "arrow.trianglehead.2.clockwise")
+                                .font(.system(size: 30, weight: .medium))
+                                .foregroundStyle(Color.white)
+                        }
+                        .frame(maxWidth: .infinity, alignment: .trailing)
+                        .padding(.trailing, 50)
                     }
-                    .frame(width: 80, height: 80)
-                    .background(
-                        Circle()
-                            .stroke(Color.white, lineWidth: 5)
-                    )
-                    .padding(.bottom, 60)
+                    .frame(height: 140, alignment: .top)
+                    .frame(maxWidth: .infinity)
                 }
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
             }
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
-        
+        .background(Color.black.ignoresSafeArea())
     }
     
     
