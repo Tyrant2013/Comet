61da2a8fa4bd595872b56c2f252da240a8b8aad7
Zhuang Xiaowei <zhuangxiaowei_dev@163.com>
2026-03-28 21:00:10 +0800

    一个编译失败的修复

--- Comet Camera/Comet Camera/CMCropViewController/Constants/CMCropViewConstants.swift
+++ Comet Camera/Comet Camera/CMCropViewController/Constants/CMCropViewConstants.swift
@@ -11,18 +11,18 @@ import UIKit
     case top
 }
 
-//public func CM_CROP_VIEW_RESOURCE_BUNDLE_FOR_OBJECT(_ object: NSObject) -> Bundle {
-//    #if SWIFT_PACKAGE
-//    let bundleName = "CMCropViewController_CMCropViewController"
-//    #else
-//    let bundleName = "CMCropViewControllerBundle"
-//    #endif
-//
-//    let classBundle = Bundle(for: type(of: object))
-//    if let resourceBundleURL = classBundle.url(forResource: bundleName, withExtension: "bundle"),
-//       let resourceBundle = Bundle(url: resourceBundleURL) {
-//        return resourceBundle
-//    }
-//
-//    return classBundle
-//}
+public func CM_CROP_VIEW_RESOURCE_BUNDLE_FOR_OBJECT(_ object: NSObject) -> Bundle {
+    #if SWIFT_PACKAGE
+    let bundleName = "CMCropViewController_CMCropViewController"
+    #else
+    let bundleName = "CMCropViewControllerBundle"
+    #endif
+
+    let classBundle = Bundle(for: type(of: object))
+    if let resourceBundleURL = classBundle.url(forResource: bundleName, withExtension: "bundle"),
+       let resourceBundle = Bundle(url: resourceBundleURL) {
+        return resourceBundle
+    }
+
+    return classBundle
+}
